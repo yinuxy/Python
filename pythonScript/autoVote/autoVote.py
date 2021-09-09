91a1916bb655b319ddf0c338cba845ace71af2cd
yinuxy <1154418252@qq.com>
2021-09-09 15:47:03 +0800

    Update

--- pythonScript/autoVote/autoVote.py
+++ pythonScript/autoVote/autoVote.py
@@ -1,11 +1,26 @@
 import time
 import random
 import requests
+import getCookie
 from lxml import etree
 
+def geySkey(cookie):
+    arrcookie = cookie.split("; ")
+    for i in range(len(arrcookie)):
+        arr = arrcookie[i].split("=")
+        if(arr[0] == 'skey'):
+            print(arr[1])
+            return arr[1]
 
-def dailyTaskAutuComiit(header_dict, vote_url, comment_url):
-    base_url = "https://cloud.tencent.com/developer/ask"
+def getGTK(skey):
+    skey=geySkey(skey)
+    hash = 5381
+    for i in range(len(skey)):
+        hash = hash + (hash << 5) + int(ord(skey[i]))
+    return (hash & 0x7fffffff)
+
+def dailyTaskAutuComiit(header_dict, vote_url, comment_url, signInurl):
+    base_url = "https://cloud.tencent.com/developer/ask?q=timeline"
     header = {
         "Accept": "application/json, text/plain, */*",
         "Accept-Encoding": "gzip, deflate, br",
@@ -34,15 +49,15 @@ def dailyTaskAutuComiit(header_dict, vote_url, comment_url):
         commentpayload = "{\r\n  \"action\": \"CommentAnswer\",\r\n  \"payload\": {\r\n    \"questionId\": %s,\r\n    \"answerId\": %s,\r\n    \"content\": \"%s\"\r\n  }\r\n}"%(key, value, '666')
         votepayloadList.append(votepayload)
         commentpayloadList.append(commentpayload)
-    index = random.sample(range(1,20),12)
+    index = random.sample(range(1,20),5)
     index.sort()
     for i in index:
         r1 = requests.request("POST", vote_url, headers=header_dict, data=votepayloadList[i])
         print("第{}篇文章已点赞，返回代码：".format(i),r1.text)
-        time.sleep(random.randint(2,5))
-        r2 = requests.request("POST", comment_url, headers=header_dict, data=commentpayloadList[i])
-        print("第{}篇文章已评论，返回代码：".format(i),r2.text)
-        time.sleep(random.randint(2,5))
+        time.sleep(random.randint(5,10))
+        # r2 = requests.request("POST", comment_url, headers=header_dict, data=commentpayloadList[i])
+        # print("第{}篇文章已评论，返回代码：".format(i),r2.text)
+        # time.sleep(random.randint(5,10))
 
 def getComment():
     commentsList = [
@@ -65,10 +80,15 @@ def getComment():
 
 
 if __name__ == '__main__':
+    getCookie.init()
+    getCookie.login()
+    time.sleep(10)
     with open("cookie.txt", "r", encoding="utf-8") as f:
         cookie = f.read()
-    vote_url = "https://cloud.tencent.com/developer/services/ajax/ask/answer?action=VoteAnswer&uin=100004697298&csrfCode=817780516"
-    comment_url = "https://cloud.tencent.com/developer/services/ajax/ask/answer?action=CommentAnswer&uin=100004697298&csrfCode=817780516"
+    csrfCode = getGTK(cookie)
+    signInurl = "https://cloud.tencent.com/developer/services/ajax/grocery-stall?action=SignIn&uin=100004697298&csrfCode=%s"%(csrfCode)
+    vote_url = "https://cloud.tencent.com/developer/services/ajax/ask/answer?action=VoteAnswer&uin=100004697298&csrfCode=%s"%(csrfCode)
+    comment_url = "https://cloud.tencent.com/developer/services/ajax/ask/answer?action=CommentAnswer&uin=100004697298&csrfCode=%s"%(csrfCode)
     header_dict = {
         'accept': 'application/json, text/plain, */*',
         'accept-encoding': 'gzip, deflate, br',
@@ -79,6 +99,7 @@ if __name__ == '__main__':
         'referer': 'https://cloud.tencent.com/developer/ask',
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36'
     }
-    dailyTaskAutuComiit(header_dict, vote_url, comment_url)
+    dailyTaskAutuComiit(header_dict, vote_url, comment_url, signInurl)
+    # print(geySkey(cookie))
 
 
